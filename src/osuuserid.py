f7e6df1d1fd9fff3f99c04e3cd3841edf14b18e1
Kevin Mankowski <mankowskikevin@gmail.com>
2020-09-30 21:02:05 -0400

    Repackage script into distinct functions.

--- src/osuuserid.py
+++ src/osuuserid.py
@@ -6,8 +6,6 @@
     Note. 'osu.ppy.sh' was not scraped directly as a user must be
            logged in to do so. '''
 
-# TODO Refactor the code into understandable and clear functions.
-
 import sys
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
@@ -15,59 +13,85 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
-# Setup webdriver ('PATH' is dependent on local machine).
-PATH = '/Users/owner/Desktop/chromedriver'
-browser = webdriver.Chrome(PATH)
 
-# UserName for which the 'Osu!' ID will be found.
-userToFind = input('Please enter an Osu! user to get their ID: ')
+def main():
+    # Setup webdriver ('PATH' is dependent on local machine).
+    PATH = '/Users/owner/Desktop/chromedriver'
+    browser = webdriver.Chrome(PATH)
 
-# Gets the Google search results for '<UserName> Osu!'.
-browser.get('https://www.google.com')
-searchbar = browser.find_element_by_name('q')
-searchbar.send_keys(userToFind + ' Osu!')
-searchbar.send_keys(Keys.RETURN)
+    # UserName for which the 'Osu!' ID will be found.
+    osuUserName = input('Please enter an Osu! username to get their ID: ')
+    # Get Osu user ID.
+    osuUserAndID = getID(osuUserName, browser)
 
-# Parses out the actual content of the search results on Google,
-# ensuring that the page is given proper time to load before searching.
-try:
-    searchcontent = WebDriverWait(browser, 10).until(
-        EC.presence_of_element_located((By.ID, 'rso'))
-    )
-except:
-    print('Driver could not find Google searchcontent in time (load speed took too long).')
+    if osuUserAndID == None:
+        print("Sorry, we couldn't find that user!")
+    else:
+        print('The user ID of ' + osuUserAndID[0] + ' is ' + osuUserAndID[1])
+
+    # Clean up.
     browser.quit()
+    sys.exit()
 
-# A list of the search results on the first page given by Google.
-searchResults = searchcontent.find_elements_by_class_name('g')
 
-# Loops through each search result to check if it contains the page of the requested user.
-for searchResult in searchResults:
-    urlTag = searchResult.find_element_by_tag_name('a')
-    urlString = urlTag.get_attribute('href')
-    if urlString.startswith('https://osu.ppy.sh/users/'):
-        browser.execute_script("window.open('');")
-        browser.switch_to.window(browser.window_handles[1])
-        browser.get(urlString)
-        userName = browser.find_element_by_class_name(
-            'profile-info__name').find_element_by_tag_name('span').text
-        isCorrectUser = ''
-        while isCorrectUser != 'y' and isCorrectUser != 'n':
-            isCorrectUser = input(
-                'Is ' + userName + ' the user you were looking for? (y/n)')
-        if isCorrectUser == 'y':
-            urlSlices = browser.current_url.split('/')
-            userID = urlSlices[len(urlSlices)-1]
-            print('The user ID of ' + userName + ' is ' + userID)
-            browser.quit()
-            sys.exit()
-        elif isCorrectUser == 'n':
-            print("Okay, we'll look for another!")
-            browser.close()
-            browser.switch_to.window(browser.window_handles[0])
+def getID(userName, browser):
+    searchResults = googleSearch(userName, browser)
+    osuUserAndID = osuScan(searchResults, browser)
+    return osuUserAndID
 
-print("Sorry, we couldn't find that Osu! user.")
 
-# Clean up.
-browser.quit()
-sys.exit()
+def googleSearch(userName, browser):
+    # Gets the Google search results for '<UserName> Osu!'.
+    browser.get('https://www.google.com')
+    searchbar = browser.find_element_by_name('q')
+    searchbar.send_keys(userName + ' Osu!')
+    searchbar.send_keys(Keys.RETURN)
+
+    # Parses out the actual content of the search results on Google,
+    # ensuring that the page is given proper time to load before searching.
+    try:
+        searchcontent = WebDriverWait(browser, 10).until(
+            EC.presence_of_element_located((By.ID, 'rso'))
+        )
+    except:
+        print(
+            'Driver could not find Google searchcontent in time (load speed took too long).')
+        browser.quit()
+        sys.exit()
+
+    # A list of the search results on the first page given by Google.
+    searchResults = searchcontent.find_elements_by_class_name('g')
+    return searchResults
+
+
+def osuScan(searchResults, browser):
+    # Loops through each search result to check if it contains the page of the requested user.
+    for searchResult in searchResults:
+        urlTag = searchResult.find_element_by_tag_name('a')
+        urlString = urlTag.get_attribute('href')
+        if urlString.startswith('https://osu.ppy.sh/users/'):
+            browser.execute_script("window.open('');")
+            browser.switch_to.window(
+                browser.window_handles[len(browser.window_handles)-1])
+            browser.get(urlString)
+            userName = browser.find_element_by_class_name(
+                'profile-info__name').find_element_by_tag_name('span').text
+            isCorrectUser = ''
+            while isCorrectUser != 'y' and isCorrectUser != 'n':
+                isCorrectUser = input(
+                    'Is ' + userName + ' the user you were looking for? (y/n)')
+            if isCorrectUser == 'y':
+                urlSlices = browser.current_url.split('/')
+                userID = urlSlices[len(urlSlices)-1]
+                return (userName, userID)
+            elif isCorrectUser == 'n':
+                print("Okay, we'll look for another!")
+                browser.close()
+                browser.switch_to.window(
+                    browser.window_handles[len(browser.window_handles)-1])
+    return None
+
+
+# Runs basic ID script if this file is being directly executed.
+if __name__ == '__main__':
+    main()
